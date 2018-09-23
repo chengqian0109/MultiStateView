02175e3d6fecf1583f721e4ce3d362512f2eecd6
chengqian0109 <chengqian9119@126.comm>
2018-09-23 11:30:05 +0800

    Initial commit

---
+++ MultiStateView-Library/src/main/java/com/jack/view/MultiStateView.java
@@ -0,0 +1,482 @@
+package com.jack.view;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.IntDef;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 页面通用加载布局
+ *
+ * @author chengqian
+ * @date 2018/5/20
+ */
+public class MultiStateView extends FrameLayout {
+
+    /**
+     * 未知状态，默认状态
+     */
+    public static final int STATE_UNKNOWN = -1;
+
+    /**
+     * 显示成功数据的状态（非空数据）
+     */
+    public static final int STATE_CONTENT = 0;
+    /**
+     * 错误状态
+     */
+    public static final int STATE_ERROR = 1;
+    /**
+     * 数据为空的状态
+     */
+    public static final int STATE_EMPTY = 2;
+    /**
+     * 正在加载中的状态
+     */
+    public static final int STATE_LOADING = 3;
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({STATE_UNKNOWN, STATE_CONTENT, STATE_ERROR, STATE_EMPTY, STATE_LOADING})
+    private @interface ViewState {
+    }
+
+    private LayoutInflater mInflater;
+
+    private View mContentView;
+
+    private View mLoadingView;
+
+    private View mErrorView;
+
+    private View mEmptyView;
+
+    private boolean mAnimateViewChanges = false;
+
+    @Nullable
+    private OnStateChangeListener mOnStateChangeListener;
+
+    @ViewState
+    private int mViewState = STATE_UNKNOWN;
+
+    public MultiStateView(Context context) {
+        this(context, null);
+    }
+
+    public MultiStateView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public MultiStateView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        mInflater = LayoutInflater.from(getContext());
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MultiStateView);
+
+        int loadingViewResId = a.getResourceId(R.styleable.MultiStateView_msv_loadingView, -1);
+        if (loadingViewResId > -1) {
+            mLoadingView = mInflater.inflate(loadingViewResId, this, false);
+            addView(mLoadingView, mLoadingView.getLayoutParams());
+        }
+
+        int emptyViewResId = a.getResourceId(R.styleable.MultiStateView_msv_emptyView, -1);
+        if (emptyViewResId > -1) {
+            mEmptyView = mInflater.inflate(emptyViewResId, this, false);
+            addView(mEmptyView, mEmptyView.getLayoutParams());
+        }
+
+        int errorViewResId = a.getResourceId(R.styleable.MultiStateView_msv_errorView, -1);
+        if (errorViewResId > -1) {
+            mErrorView = mInflater.inflate(errorViewResId, this, false);
+            addView(mErrorView, mErrorView.getLayoutParams());
+        }
+
+        int viewState = a.getInt(R.styleable.MultiStateView_msv_viewState, STATE_CONTENT);
+        mAnimateViewChanges = a.getBoolean(R.styleable.MultiStateView_msv_animateViewChanges, false);
+        switch (viewState) {
+            case STATE_CONTENT:
+                mViewState = STATE_CONTENT;
+                break;
+
+            case STATE_ERROR:
+                mViewState = STATE_ERROR;
+                break;
+
+            case STATE_EMPTY:
+                mViewState = STATE_EMPTY;
+                break;
+
+            case STATE_LOADING:
+                mViewState = STATE_LOADING;
+                break;
+
+            case STATE_UNKNOWN:
+            default:
+                mViewState = STATE_UNKNOWN;
+                break;
+        }
+
+        a.recycle();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mContentView == null) {
+            throw new IllegalArgumentException("Content view is not defined");
+        }
+        setView(STATE_UNKNOWN);
+    }
+
+    @Override
+    public void addView(View child) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        super.addView(child);
+    }
+
+    @Override
+    public void addView(View child, int index) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        super.addView(child, index);
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        super.addView(child, index, params);
+    }
+
+    @Override
+    public void addView(View child, ViewGroup.LayoutParams params) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        super.addView(child, params);
+    }
+
+    @Override
+    public void addView(View child, int width, int height) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        super.addView(child, width, height);
+    }
+
+    @Override
+    protected boolean addViewInLayout(View child, int index, ViewGroup.LayoutParams params) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        return super.addViewInLayout(child, index, params);
+    }
+
+    @Override
+    protected boolean addViewInLayout(View child, int index, ViewGroup.LayoutParams params, boolean preventRequestLayout) {
+        if (isValidContentView(child)) {
+            mContentView = child;
+        }
+        return super.addViewInLayout(child, index, params, preventRequestLayout);
+    }
+
+    /**
+     * Returns the {@link View} associated with the {@link ViewState}
+     *
+     * @param state The {@link ViewState} with to return the view for
+     * @return The {@link View} associated with the {@link ViewState}, null if no view is present
+     */
+    @Nullable
+    public View getView(@ViewState int state) {
+        switch (state) {
+            case STATE_LOADING:
+                return mLoadingView;
+
+            case STATE_CONTENT:
+                return mContentView;
+
+            case STATE_EMPTY:
+                return mEmptyView;
+
+            case STATE_ERROR:
+                return mErrorView;
+
+            case STATE_UNKNOWN:
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns the current {@link ViewState}
+     *
+     * @return
+     */
+    @ViewState
+    public int getViewState() {
+        return mViewState;
+    }
+
+    /**
+     * Sets the current {@link ViewState}
+     *
+     * @param state The {@link ViewState} to set {@link MultiStateView} to
+     */
+    public void setViewState(@ViewState int state) {
+        if (state != mViewState) {
+            int previous = mViewState;
+            mViewState = state;
+            setView(previous);
+            if (mOnStateChangeListener != null) {
+                mOnStateChangeListener.onStateChanged(mViewState);
+            }
+        }
+    }
+
+    /**
+     * Shows the {@link View} based on the {@link ViewState}
+     */
+    private void setView(@ViewState int previousState) {
+        switch (mViewState) {
+            case STATE_LOADING:
+                if (mLoadingView == null) {
+                    throw new NullPointerException("Loading View");
+                }
+                hideView();
+                mLoadingView.setVisibility(View.VISIBLE);
+                if (mAnimateViewChanges) {
+                    animateLayoutChange(getView(previousState));
+                } else {
+                    mLoadingView.setVisibility(View.VISIBLE);
+                }
+                break;
+
+            case STATE_EMPTY:
+                if (mEmptyView == null) {
+                    throw new NullPointerException("Empty View");
+                }
+                hideView();
+                mEmptyView.setVisibility(View.VISIBLE);
+                if (mAnimateViewChanges) {
+                    animateLayoutChange(getView(previousState));
+                } else {
+                    mEmptyView.setVisibility(View.VISIBLE);
+                }
+                break;
+
+            case STATE_CONTENT:
+                if (mContentView == null) {
+                    throw new NullPointerException("Content View");
+                }
+                hideView();
+                mContentView.setVisibility(View.VISIBLE);
+                if (mAnimateViewChanges) {
+                    animateLayoutChange(getView(previousState));
+                } else {
+                    mContentView.setVisibility(View.VISIBLE);
+                }
+                break;
+            default:
+            case STATE_ERROR:
+                if (mErrorView == null) {
+                    throw new NullPointerException("Error View");
+                }
+                hideView();
+                mErrorView.setVisibility(View.VISIBLE);
+                if (mAnimateViewChanges) {
+                    animateLayoutChange(getView(previousState));
+                } else {
+                    mErrorView.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+    }
+
+    /**
+     * 隐藏View对象
+     */
+    private void hideView() {
+        if (mLoadingView != null) {
+            mLoadingView.setVisibility(View.GONE);
+        }
+        if (mErrorView != null) {
+            mErrorView.setVisibility(View.GONE);
+        }
+        if (mEmptyView != null) {
+            mEmptyView.setVisibility(View.GONE);
+        }
+        if (mContentView != null) {
+            mContentView.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Checks if the given {@link View} is valid for the Content View
+     *
+     * @param view The {@link View} to check
+     * @return
+     */
+    private boolean isValidContentView(View view) {
+        if (mContentView != null && mContentView != view) {
+            return false;
+        }
+
+        return view != mLoadingView && view != mErrorView && view != mEmptyView;
+    }
+
+    /**
+     * Sets the view for the given view state
+     *
+     * @param view          The {@link View} to use
+     * @param state         The {@link ViewState}to set
+     * @param switchToState If the {@link ViewState} should be switched to
+     */
+    public void setViewForState(View view, @ViewState int state, boolean switchToState) {
+        switch (state) {
+            case STATE_LOADING:
+                if (mLoadingView != null) {
+                    removeView(mLoadingView);
+                }
+                mLoadingView = view;
+                addView(mLoadingView);
+                break;
+
+            case STATE_EMPTY:
+                if (mEmptyView != null) {
+                    removeView(mEmptyView);
+                }
+                mEmptyView = view;
+                addView(mEmptyView);
+                break;
+
+            case STATE_ERROR:
+                if (mErrorView != null) {
+                    removeView(mErrorView);
+                }
+                mErrorView = view;
+                addView(mErrorView);
+                break;
+
+            case STATE_CONTENT:
+                if (mContentView != null) {
+                    removeView(mContentView);
+                }
+                mContentView = view;
+                addView(mContentView);
+                break;
+
+            case STATE_UNKNOWN:
+            default:
+        }
+
+        setView(STATE_UNKNOWN);
+        if (switchToState) {
+            setViewState(state);
+        }
+    }
+
+    /**
+     * Sets the {@link View} for the given {@link ViewState}
+     *
+     * @param view  The {@link View} to use
+     * @param state The {@link ViewState} to set
+     */
+    public void setViewForState(View view, @ViewState int state) {
+        setViewForState(view, state, false);
+    }
+
+    /**
+     * Sets the {@link View} for the given {@link ViewState}
+     *
+     * @param layoutRes     Layout resource id
+     * @param state         The {@link ViewState} to set
+     * @param switchToState If the {@link ViewState} should be switched to
+     */
+    public void setViewForState(@LayoutRes int layoutRes, @ViewState int state, boolean switchToState) {
+        if (mInflater == null) {
+            mInflater = LayoutInflater.from(getContext());
+        }
+        View view = mInflater.inflate(layoutRes, this, false);
+        setViewForState(view, state, switchToState);
+    }
+
+    /**
+     * Sets the {@link View} for the given {@link ViewState}
+     *
+     * @param layoutRes Layout resource id
+     * @param state     The {@link View} state to set
+     */
+    public void setViewForState(@LayoutRes int layoutRes, @ViewState int state) {
+        setViewForState(layoutRes, state, false);
+    }
+
+    /**
+     * Sets whether an animate will occur when changing between {@link ViewState}
+     *
+     * @param animate
+     */
+    public void setAnimateLayoutChanges(boolean animate) {
+        mAnimateViewChanges = animate;
+    }
+
+    /**
+     * Sets the {@link OnStateChangeListener} for the view
+     *
+     * @param listener The {@link OnStateChangeListener} that will receive callbacks
+     */
+    public void setOnStateChangeListener(OnStateChangeListener listener) {
+        mOnStateChangeListener = listener;
+    }
+
+    /**
+     * Animates the layout changes between {@link ViewState}
+     *
+     * @param previousView The view that it was currently on
+     */
+    private void animateLayoutChange(@Nullable final View previousView) {
+        if (previousView == null) {
+            getView(mViewState).setVisibility(View.VISIBLE);
+            return;
+        }
+
+        previousView.setVisibility(View.VISIBLE);
+        ObjectAnimator anim = ObjectAnimator.ofFloat(previousView, "alpha", 1.0f, 0.0f).setDuration(250L);
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                previousView.setVisibility(View.GONE);
+                getView(mViewState).setVisibility(View.VISIBLE);
+                ObjectAnimator.ofFloat(getView(mViewState), "alpha", 0.0f, 1.0f).setDuration(250L).start();
+            }
+        });
+        anim.start();
+    }
+
+    public interface OnStateChangeListener {
+        /**
+         * Callback for when the {@link ViewState} has changed
+         *
+         * @param viewState The {@link ViewState} that was switched to
+         */
+        void onStateChanged(@ViewState int viewState);
+    }
+}
---
+++ app/src/main/java/com/jack/multistateview/MainActivity.java
@@ -0,0 +1,61 @@
+package com.jack.multistateview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.jack.view.MultiStateView;
+
+public class MainActivity extends AppCompatActivity implements MultiStateView.OnStateChangeListener {
+
+    private MultiStateView mStateView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mStateView = findViewById(R.id.state_view);
+        mStateView.setOnStateChangeListener(this);
+    }
+
+    public void showContentView(View view) {
+        mStateView.setViewState(MultiStateView.STATE_CONTENT);
+    }
+
+    public void showEmptyView(View view) {
+        mStateView.setViewState(MultiStateView.STATE_EMPTY);
+    }
+
+    public void showErrorView(View view) {
+        mStateView.setViewState(MultiStateView.STATE_ERROR);
+    }
+
+    public void showLoadingView(View view) {
+        mStateView.setViewState(MultiStateView.STATE_LOADING);
+    }
+
+    @Override
+    public void onStateChanged(int viewState) {
+        String state;
+        switch (viewState) {
+            case MultiStateView.STATE_CONTENT:
+                state = "STATE_CONTENT";
+                break;
+            case MultiStateView.STATE_EMPTY:
+                state = "STATE_EMPTY";
+                break;
+            case MultiStateView.STATE_ERROR:
+                state = "STATE_ERROR";
+                break;
+            case MultiStateView.STATE_LOADING:
+                state = "STATE_LOADING";
+                break;
+            case MultiStateView.STATE_UNKNOWN:
+            default:
+                state = "STATE_UNKNOWN";
+                break;
+        }
+        Toast.makeText(this, "State changed,current state is " + state, Toast.LENGTH_SHORT).show();
+    }
+}
